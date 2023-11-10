0965312aa35f47e478f40ab3da52f7aa9c3f5f6d
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-11 01:05:54 +0530

    Not working

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,10 +4,37 @@ import "./index.css";
 import "./styles/icons/icons.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux"
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from "redux-devtools-extension"
+import rootReducer from "./reducers/index";
+
+const store = createStore(rootReducer, composeWithDevTools())
+
+/*
+@deprecated
+We recommend using the configureStore method of the @reduxjs/toolkit package, which replaces createStore.
+
+Redux Toolkit is our recommended approach for writing Redux logic today, including store setup, reducers, data fetching, and more.
+
+For more details, please read this Redux docs page: https://redux.js.org/introduction/why-rtk-is-redux-today
+
+configureStore from Redux Toolkit is an improved version of createStore that simplifies setup and helps avoid common bugs.
+
+configureStore from Redux Toolkit is an improved version of createStore that simplifies setup and helps avoid common bugs.
+
+You should not be using the redux core package by itself today, except for learning purposes. The createStore method from the core redux package will not be removed, but we encourage all users to migrate to using Redux Toolkit for all Redux code.
+
+If you want to use createStore without this visual deprecation warning, use the legacy_createStore import instead:
+
+import { legacy_createStore as createStore} from 'redux'
+*/
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
