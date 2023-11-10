26391a07950d639b64830a43de23c342655d3fd8
Ujwal KC <ujwalkcsps@gmail.com>
2023-11-10 23:36:33 +0530

    initial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,6 @@
+import { Routes, Route } from "react-router-dom";
+
 function App() {
-
-  // Asynchronus call to backend services
-  const get = async() => {
-
-    // wait for sme time and fetch the results
-    const res = await fetch('http://localhost:8000/api');
-
-    // If good result will be 200 OK
-    console.log(res);
-  };
-
-  // Calling The function
-  get();
-  
-  return <div>welcome to frontend</div>;
 }
 
 export default App;
